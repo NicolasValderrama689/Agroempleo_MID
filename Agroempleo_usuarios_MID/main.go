65e530beeb7961a877f4d8b160d18f431acbe969
nicolasvalderrama689 <nicolasvalderrama689@gmail.com>
2025-03-20 16:53:24 -0500

    subir repo mid

---
+++ Agroempleo_usuarios_MID/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/sena_2824182/Agroempleo_MID/Agroempleo_usuarios_MID/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
